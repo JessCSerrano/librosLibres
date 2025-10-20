7c613e2611c05d6caa4e103e1e48e41becfbfb48
jess.cserrano <jess.cserrano@gmail.com>
2025-10-20 13:36:28 +0200

    feat: initial book development cleanup

--- librosLibres/src/main/java/com/jessCSerrano/librosLibres/application/book/BookService.java
+++ librosLibres/src/main/java/com/jessCSerrano/librosLibres/application/book/BookService.java
@@ -1,11 +1,6 @@
 package com.jessCSerrano.librosLibres.application.book;
 
-import com.jessCSerrano.librosLibres.adapters.persistence.entity.book.BookEntity;
-import com.jessCSerrano.librosLibres.domain.ports.in.book.GetAllBooksUseCase;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Implementation of GetAllBookTitlesUseCase
@@ -15,13 +10,6 @@ import java.util.List;
  * Dependencies: BookRepository
  */
 @Service
-public class BookService implements GetAllBooksUseCase {
-    @Override
-    public List<BookEntity> getAllBooks() {
-        var elPrincipito = new BookEntity();
-        elPrincipito.setTitle("El principito");
-        var harryPotter = new BookEntity();
-        harryPotter.setTitle("Harry Potter");
-        return new ArrayList<>(List.of(elPrincipito, harryPotter));
-    }
+public class BookService  {
+
 }
